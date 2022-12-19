2c71ed700b8773499ca3fb87fe7b8494f98c12a3
yto-tkg <yuto-takagi@cybozu.co.jp>
2022-12-20 08:46:30 +0900

    feature initialization

---
+++ src/main/java/com/example/common/processing/SpringCommonProcessingApplication.java
@@ -0,0 +1,13 @@
+package com.example.common.processing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCommonProcessingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCommonProcessingApplication.class, args);
+	}
+
+}
